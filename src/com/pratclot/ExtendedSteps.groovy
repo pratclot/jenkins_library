cffc542319469f781b05a0098283079d37c9a133
chief <pratclot@live.ru>
2020-05-18 16:18:33 +0300

    refactor ExtendedSteps and Slack classes

--- src/com/pratclot/ExtendedSteps.groovy
+++ src/com/pratclot/ExtendedSteps.groovy
@@ -3,29 +3,34 @@ package com.pratclot
 class ExtendedSteps implements Serializable{
 
     private callerScript
+    private String lastStage
 
     ExtendedSteps(callerScript) {
         this.callerScript = callerScript
     }
 
     def call(shellCommands) {
+        lastStage = callerScript.env.STAGE_NAME
         updateGitHubStatus("pending")
-        String lastStage = callerScript.env.STAGE_NAME
-        String ts = callerScript.slack.updateThread("Starting ${lastStage} stage...").ts
+        String ts = updateSlackStatus("Starting ${lastStage} stage...").ts
         try {
             callerScript.sh """
                 ${shellCommands}
             """
             updateGitHubStatus("success")
-            callerScript.slack.updateThread("${lastStage} succeeded", "good", ts)
+            updateSlackStatus("good", "${lastStage} succeeded", ts)
         } catch(ex) {
             updateGitHubStatus("failure")
-            callerScript.slack.updateThread("${lastStage} failed", "danger", ts)
+            updateSlackStatus("danger", "${lastStage} failed", ts)
             throw ex
         }
     }
 
-    void updateGitHubStatus(String status) {
-        callerScript.println(GitHub.setBuildStatus(callerScript, "blah-blah", status, callerScript.env.STAGE_NAME))
+    private Object updateSlackStatus(String status = null, String message, String ts = null) {
+        return callerScript.slack.updateThread(message, status, ts)
+    }
+
+    private void updateGitHubStatus(String status, String message = "") {
+        callerScript.println(GitHub.setBuildStatus(callerScript, message, status, lastStage))
     }
 }
